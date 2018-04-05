7376547833d90e647b28bce7cea8d2de05e57783
JustinChowBeliefChain <zhouzhiyu@beliefchain.com>
2018-04-06 01:59:59 +0800

    using official address as default download path

--- go-workspace/download_golang.sh
+++ go-workspace/download_golang.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-wget http://filecache.codeplanter.com/vagrant/go1.10.1.linux-amd64.tar.gz 
+wget https://dl.google.com/go/go1.10.1.linux-amd64.tar.gz
+
+#wget http://filecache.codeplanter.com/vagrant/go1.10.1.linux-amd64.tar.gz 
